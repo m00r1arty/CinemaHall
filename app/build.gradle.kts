0a1a77463e74d54cc1ddd4aaf1e0ad4d09d01ff7
Ikrom <ikromjon.rajabov@alif.tj>
2025-08-08 10:52:55 +0500

    changed sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "tj.ikrom.cinemahall"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "tj.ikrom.cinemahall"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
