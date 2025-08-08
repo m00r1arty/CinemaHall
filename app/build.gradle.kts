955ba1202166207abc6f1672bbf9e236ec3739ea
Ikrom <ikromjon.rajabov@alif.tj>
2025-08-08 12:05:16 +0500

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
@@ -36,6 +37,10 @@ android {
 }
 
 dependencies {
+
+    implementation(libs.okhttp3)
+    implementation(libs.glide)
+    kapt(libs.glide.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
