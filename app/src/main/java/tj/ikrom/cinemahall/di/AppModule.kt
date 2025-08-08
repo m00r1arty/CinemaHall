ffc88b7535559f8ec0181e90944d77a648ccf1f5
Ikrom <ikromjon.rajabov@alif.tj>
2025-08-08 12:05:46 +0500

    added App Module for DI

---
+++ app/src/main/java/tj/ikrom/cinemahall/di/AppModule.kt
@@ -0,0 +1,4 @@
+package tj.ikrom.cinemahall.di
+
+object AppModule {
+}
--- app/src/main/java/tj/ikrom/cinemahall/ui/MainActivity.kt
+++ app/src/main/java/tj/ikrom/cinemahall/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package tj.ikrom.cinemahall
+package tj.ikrom.cinemahall.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import tj.ikrom.cinemahall.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
